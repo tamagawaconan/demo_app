3864d1158a0295189cac27af31a3a67d4c20d4f8
Kazuhiro Kawachi <tamagawaconan@gmail.com>
2015-06-03 23:05:16 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '00c828451bd3a029fece4188f4f1a2a7781f6088367322b17a09fd91c95c45d3dd26a6c5887637e142092dae2cf9b4721d4b8577a41b530fc606f3d97d9348d1'
---
+++ gems/gems/json-1.8.3/ext/json/ext/parser/siteconf20150603-46922-hfujha.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150603-46922-1l02rpz"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
